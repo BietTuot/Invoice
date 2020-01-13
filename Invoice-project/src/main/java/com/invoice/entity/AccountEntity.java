46a4569ed2172a5cf8227cc086de3e0e81df8355
BietTuot <BietTuot@gmail.com>
2020-01-13 16:31:50 +0100

    Update Entity

--- Invoice-project/src/main/java/com/invoice/entity/AccountEntity.java
+++ Invoice-project/src/main/java/com/invoice/entity/AccountEntity.java
@@ -1,5 +1,6 @@
 package com.invoice.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -7,4 +8,61 @@ import javax.persistence.Table;
 @Table(name = "account")
 public class AccountEntity extends BaseEntity{
 
+	@Column(name="providerid")
+	private int providerId;
+	
+	@Column(name="customerid")
+	private int customerId;
+	
+	@Column(name="bic")
+	private String bic;
+	
+	@Column(name="iban")
+	private String iban;
+	
+	@Column(name="bankid")
+	private String bankId;
+
+	public int getProviderId() {
+		return providerId;
+	}
+
+	public void setProviderId(int providerId) {
+		this.providerId = providerId;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getBic() {
+		return bic;
+	}
+
+	public void setBic(String bic) {
+		this.bic = bic;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public String getBankId() {
+		return bankId;
+	}
+
+	public void setBankId(String bankId) {
+		this.bankId = bankId;
+	}
+
+	
+	
 }
